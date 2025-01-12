e1462d1efc3878e4ad471c117150224d0528f541
Irina Makarova <mario-2812@yandex.ru>
2025-01-12 22:49:50 +0300

    Реализует открытие мобильного меню. Сверяет мобилку по РР

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+
+const ModalMenu = document.querySelector('.header__nav');
+const ToggleElement = document.querySelector('.header__toggle-button');
+
+
+ToggleElement.addEventListener('click', () => {
+  ToggleElement.classList.toggle('header__toggle-button-open');
+  ModalMenu.classList.toggle('navigation-hidden');
+  ModalMenu.classList.toggle('navigation-show');
+});
+
